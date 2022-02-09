53beed1440e6a581ff7a3df5bbdfa7c04b52c641
timoprice <91557541+timoprice@users.noreply.github.com>
2022-02-09 12:47:40 -0600

    Update index.jsx

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -17,7 +17,7 @@ const Layout = ({ children }) => {
         viewBox="0 0 404 784"
       >
         <defs>
-          <pattern
+<!--          <pattern
             id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
             x="0"
             y="0"
@@ -33,7 +33,7 @@ const Layout = ({ children }) => {
               className="text-gray-200"
               fill="#edf2f7"
             />
-          </pattern>
+-->          </pattern>
         </defs>
         <rect
           width="404"
