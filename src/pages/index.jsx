a15894f36e191ac2e2314025b5012c93cdf1c6ec
timoprice <91557541+timoprice@users.noreply.github.com>
2022-02-09 16:57:09 -0600

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,7 @@ import SectionBlog from '../components/section-blog';
 import SectionExperience from '../components/section-experience';
 import SectionProjects from '../components/section-projects';
 import SectionSkills from '../components/section-skills';
-//import SEO from '../components/seo';
+import SEO from '../components/seo';
 
 const Index = ({ data }) => {
   const about = get(data, 'site.siteMetadata.about', false);
@@ -21,6 +21,7 @@ const Index = ({ data }) => {
 
   return (
     <Layout>
+      <SEO />
       <Header metadata={data.site.siteMetadata} noBlog={noBlog} />
       {about && <SectionAbout about={about} />}
       {projects && projects.length && <SectionProjects projects={projects} />}
