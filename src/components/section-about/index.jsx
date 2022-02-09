445858be797f9b50f8f55fc841f952f6c77d0d41
timoprice <91557541+timoprice@users.noreply.github.com>
2022-02-09 09:51:30 -0600

    Update index.jsx

--- src/components/section-about/index.jsx
+++ src/components/section-about/index.jsx
@@ -7,8 +7,35 @@ const SectionAbout = ({ about }) => {
     <Section title="About Me">
       <div className="mb-6">
         <p>{about}</p>
+      </div>
+    </Section>
+  );
+};
+
+const SectionAbout = ({ about2 }) => {
+  return (
+    <Section title="About Me">
+      <div className="mb-6">
         <p>{about2}</p>
+      </div>
+    </Section>
+  );
+};
+
+const SectionAbout = ({ about3 }) => {
+  return (
+    <Section title="About Me">
+      <div className="mb-6">
         <p>{about3}</p>
+      </div>
+    </Section>
+  );
+};
+
+const SectionAbout = ({ about4 }) => {
+  return (
+    <Section title="About Me">
+      <div className="mb-6">
         <p>{about4}</p>
       </div>
     </Section>
