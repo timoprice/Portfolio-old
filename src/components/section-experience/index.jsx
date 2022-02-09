55f01991ef7cacc0d1b1e02f9d8f27004b482170
timoprice <91557541+timoprice@users.noreply.github.com>
2022-02-09 09:43:02 -0600

    Update index.jsx

--- src/components/section-experience/index.jsx
+++ src/components/section-experience/index.jsx
@@ -7,7 +7,7 @@ const SectionExperience = ({ experience }) => {
   if (!experience.length) return null;
 
   return (
-    <Section title="Experience">
+    <Section title="Selected Experience">
       {experience.map((item) => (
         <SummaryItem
           key={item.name}
