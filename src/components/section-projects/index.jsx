cc4f3338fd1fbb694efe1ab69f5b32993a8dbba0
timoprice <91557541+timoprice@users.noreply.github.com>
2022-03-28 10:59:08 -0500

    Update index.jsx

--- src/components/section-projects/index.jsx
+++ src/components/section-projects/index.jsx
@@ -14,6 +14,7 @@ const SectionProjects = ({ projects }) => {
           name={project.name}       
           description={project.description}
           link={project.link}
+        />
       ))}
     </Section>
   );
